5c0accba43c4a19414982fe9670b8db9f8a054b0
jscblack <jscblack@163.com>
2022-06-07 15:29:31 +0800

    add sensor display part

--- client.py
+++ client.py
@@ -1,32 +1,45 @@
 '''
 Author       : Gehrychiang
-LastEditTime : 2022-06-07 12:49:55
+LastEditTime : 2022-06-07 15:20:26
 Website      : www.yilantingfeng.site
 E-mail       : gehrychiang@aliyun.com
 '''
 #客户端
 import tkinter as tk
-import tkinter.ttk as ttk
 import queue
 import socket
 import threading
-from turtle import back, color
 import cv2
+from matplotlib.font_manager import json_load
 import numpy as np
 import time
 import json
 from PIL import Image, ImageTk
 # config area
-vid_port=18081
-cmd_port=18082
-localhost='127.0.0.1'
-ip_addr=localhost
+vid_port = 18081
+cmd_port = 18082
+localhost = '127.0.0.1'
+ip_addr = localhost
+cmd_list = [
+    '{"cmd":"ping","para":{}}',
+    '{"cmd":"move","para":{"direction":"forward"}}',
+    '{"cmd":"move","para":{"direction":"backward"}}',
+    '{"cmd":"move","para":{"direction":"left"}}',
+    '{"cmd":"move","para":{"direction":"right"}}',
+    '{"cmd":"chmod","para":{"to":"manual"}}',
+    '{"cmd":"chmod","para":{"to":"auto-lane"}}',
+    '{"cmd":"chmod","para":{"to":"auto-avoidance"}}',
+    '{"cmd":"stop","para":{}}',
+    '{"cmd":"status","para":{}}',
+]
 # config end
 
 # global area
-que=queue.Queue()
-cmd_que=queue.Queue()
+que = queue.Queue()
+cmd_que = queue.Queue()
+sta_que = queue.Queue()
 # global end
+
 
 def vid_downstream():
     while True:
@@ -35,7 +48,7 @@ def vid_downstream():
         print('服务端已连接')
         while True:
             try:
-                res=client.recv(262144)
+                res = client.recv(262144)
                 # print('已收到服务器信息：', res.decode('utf-8'))
                 # res=client.recv(1024)
                 # print('received',len(res))
@@ -55,33 +68,27 @@ def vid_downstream():
                 print('连接终止，尝试重启')
                 break
 
+
 def cmd_upstream():
-    # while True:
-    #     if cmd_que.empty()==False:
-    #         cmd=cmd_que.get()
-    #         print('收到命令：', cmd)
-    #     time.sleep(0.1)
     client = socket.socket()
     client.connect((ip_addr, cmd_port))
     print('服务端已连接')
     while True:
         try:
             # cmd is a json-like string
-            # {'cmd':'move','para':{'direction':'forward','speed':'0.5'}}
+            # {"cmd":"move","para":{"direction":"forward"}}
             # {'cmd':'getStatus','para':{}}
             # {"ret": 200, "data": {}}
             while True:
                 if not cmd_que.empty():
-                    cmd=cmd_que.get()
-                    cmd_d=json.dumps({"cmd":cmd,"para":{}})
-                    client.send(cmd_d.encode('utf-8'))
-                time.sleep(0.1)
-
-            # cmd_exp='{"cmd":"getStatus","para":{}}'
-            # client.send(cmd_exp.encode('utf-8'))
-            # ret=client.recv(512).decode('utf-8')
-            # ret_d=json.loads(ret)
-            # print('响应结果 ',ret_d["data"]["status"])
+                    cmd = cmd_que.get()
+                    client.send(cmd_list[cmd].encode('utf-8'))
+                    ret = client.recv(512).decode('utf-8')
+                    ret_d = json.loads(ret)
+                    print('响应结果 ', ret_d["data"])
+                    if cmd==9:
+                        sta_que.put(ret_d["data"])
+                time.sleep(0.02)
 
         except ConnectionResetError:
             print('连接断开，等待重新连接')
@@ -93,55 +100,155 @@ def cmd_upstream():
             client.close()
             break
 
+
 def graphMain():
     root = tk.Tk()
     root.title('RaspiCarSolution')
+    # root.iconbitmap('favicon.ico')
     root.geometry('1280x720')
     root.resizable(False, False)
     root.configure(background='#F0F0F0')
-    label = tk.Label(root, text='RaspiCarSolution',fg='#252526', bg='#F0F0F0', font=('Arial', 20))
+    radiobut_val = tk.IntVar()
+    radiobut_val.set(1)
+    label = tk.Label(
+        root,
+        text='RaspiCarSolution',
+        fg='#252526',
+        bg='#F0F0F0',
+        font=('Arial', 20))
     label.place(x=0, y=0, width=1280, height=50)
     vid_frame = tk.Label(root)
     vid_frame.place(x=213, y=55, width=854, height=480)
+
+    # 四个方向键按钮
+    def sendMoveCmd(event):
+        cmd_que.put(event)
+        radiobut_val.set(1)
+
+    def sendChmodCmd(event):
+        cmd_que.put(event)
+        radiobut_val.set(event - 4)
+
+    forward_but = tk.Button(
+        root, text='↑', font=('Arial', 16),
+        command=lambda: sendMoveCmd(1)).place(
+            x=213 + 50, y=550, width=50, height=50)
+    backward_but = tk.Button(
+        root, text='↓', font=('Arial', 16),
+        command=lambda: sendMoveCmd(2)).place(
+            x=213 + 50, y=550 + 100, width=50, height=50)
+    left_but = tk.Button(
+        root, text='←', font=('Arial', 16),
+        command=lambda: sendMoveCmd(3)).place(
+            x=213, y=550 + 50, width=50, height=50)
+    right_but = tk.Button(
+        root, text='→', font=('Arial', 16),
+        command=lambda: sendMoveCmd(4)).place(
+            x=213 + 100, y=550 + 50, width=50, height=50)
+    stop_but = tk.Button(
+        root, text='X', font=('Arial', 16),
+        command=lambda: sendMoveCmd(8)).place(
+            x=213 + 50, y=550 + 50, width=50, height=50)
+
+    # 按键捕获
+    root.bind('<KeyPress-Up>', lambda event: sendMoveCmd(1))
+    root.bind('<KeyPress-Down>', lambda event: sendMoveCmd(2))
+    root.bind('<KeyPress-Left>', lambda event: sendMoveCmd(3))
+    root.bind('<KeyPress-Right>', lambda event: sendMoveCmd(4))
+    root.bind('<KeyPress-w>', lambda event: sendMoveCmd(1))
+    root.bind('<KeyPress-d>', lambda event: sendMoveCmd(2))
+    root.bind('<KeyPress-a>', lambda event: sendMoveCmd(3))
+    root.bind('<KeyPress-s>', lambda event: sendMoveCmd(4))
+    root.bind('<KeyPress-x>', lambda event: sendMoveCmd(8))
+    root.bind('<KeyPress-1>', lambda event: sendChmodCmd(5))
+    root.bind('<KeyPress-2>', lambda event: sendChmodCmd(6))
+    root.bind('<KeyPress-3>', lambda event: sendChmodCmd(7))
+
+    # 运行模式选择
+    mode_label = tk.Label(
+        root,
+        text='Running Mode',
+        fg='#252526',
+        bg='#F0F0F0',
+        font=('Arial', 15))
+    mode_label.place(x=213 + 300, y=550 + 50, width=200, height=50)
+    manual_but = tk.Radiobutton(
+        root,
+        variable=radiobut_val,
+        text='Manual',
+        font=('Arial', 12),
+        value=1,
+        command=lambda: cmd_que.put(5)).place(
+            x=213 + 500, y=550 + 50, width=100, height=50)
+    auto1_but = tk.Radiobutton(
+        root,
+        variable=radiobut_val,
+        text='Auto-Lane',
+        font=('Arial', 12),
+        value=2,
+        command=lambda: cmd_que.put(6)).place(
+            x=213 + 600, y=550 + 50, width=100, height=50)
+    auto2_but = tk.Radiobutton(
+        root,
+        variable=radiobut_val,
+        text='Auto-Avoidance',
+        font=('Arial', 12),
+        value=3,
+        command=lambda: cmd_que.put(7)).place(
+            x=213 + 700, y=550 + 50, width=150, height=50)
+
+    # 温湿度模块
+    temp_label = tk.Label(
+        root,
+        text='Temperature',
+        fg='#252526',
+        bg='#F0F0F0',
+        font=('Arial', 15))
+    temp_label.place(x=213 + 860, y=150, width=120, height=50)
+    temp_val = tk.Label(
+        root, text='23.5°', fg='#252526', bg='#F0F0F0', font=('Arial', 15))
+    temp_val.place(x=213 + 980, y=150, width=90, height=50)
+    humi_label = tk.Label(
+        root, text='Humidity', fg='#252526', bg='#F0F0F0', font=('Arial', 15))
+    humi_label.place(x=213 + 860, y=200, width=120, height=50)
+    humi_val = tk.Label(
+        root, text='54.3%', fg='#252526', bg='#F0F0F0', font=('Arial', 15))
+    humi_val.place(x=213 + 980, y=200, width=90, height=50)
+
+    def sta_update():
+        cmd_que.put(9)
+        if not sta_que.empty():
+            sta=sta_que.get()
+            sta=json.loads(sta)
+            # print(sta)
+            temp_val.config(text=str(sta["temp"]) + '°')
+            humi_val.config(text=str(sta["humi"]) + '%')
+            # sta=json.loads(sta)
+            # temp=sta["temp"]
+            # humi=sta["humi"]
+            # temp_val.config(text=str(temp) + '°')
+            # humi_val.config(text=str(humi) + '%')
+        temp_val.after(3000, sta_update)
+
     def vid_update():
         if not que.empty():
-            cv2image= que.get()
+            cv2image = que.get()
             img = Image.fromarray(cv2image)
-            imgtk = ImageTk.PhotoImage(image = img)
+            imgtk = ImageTk.PhotoImage(image=img)
             vid_frame.imgtk = imgtk
             vid_frame.configure(image=imgtk)
             # 15ms后重复以连续捕获
         vid_frame.after(10, vid_update)
-    
-    # 四个方向键按钮
-    forward_but=tk.Button(root, text='↑',font=('Arial', 16), command=lambda: cmd_que.put(1)).place(x=213+50, y=550, width=50, height=50)
-    backward_but=tk.Button(root, text='↓',font=('Arial', 16), command=lambda: cmd_que.put(2)).place(x=213+50, y=550+100, width=50, height=50)
-    left_but=tk.Button(root, text='←',font=('Arial', 16), command=lambda: cmd_que.put(3)).place(x=213, y=550+50, width=50, height=50)
-    right_but=tk.Button(root, text='→',font=('Arial', 16), command=lambda: cmd_que.put(4)).place(x=213+100, y=550+50, width=50, height=50)
-    # 方向键捕获
-    root.bind('<KeyPress-Up>', lambda event: cmd_que.put(1))
-    root.bind('<KeyPress-Down>', lambda event: cmd_que.put(2))
-    root.bind('<KeyPress-Left>', lambda event: cmd_que.put(3))
-    root.bind('<KeyPress-Right>', lambda event: cmd_que.put(4))
-    root.bind('<KeyPress-w>', lambda event: cmd_que.put(1))
-    root.bind('<KeyPress-d>', lambda event: cmd_que.put(2))
-    root.bind('<KeyPress-a>', lambda event: cmd_que.put(3))
-    root.bind('<KeyPress-s>', lambda event: cmd_que.put(4))
-    # 运行模式选择
-    mode_label=tk.Label(root, text='Running Mode',fg='#252526', bg='#F0F0F0', font=('Arial', 12))
-    mode_label.place(x=213+300, y=550+50, width=200, height=50)
-    manual_but=tk.Radiobutton(root, text='Manual',font=('Arial', 12),  value=1, command=lambda: cmd_que.put(5)).place(x=213+500, y=550+50, width=100, height=50)
-    auto1_but=tk.Radiobutton(root, text='Auto-Lane',font=('Arial', 12),  value=2, command=lambda: cmd_que.put(6)).place(x=213+600, y=550+50, width=100, height=50)
-    auto2_but=tk.Radiobutton(root, text='Auto-Avoidance',font=('Arial', 12),  value=3, command=lambda: cmd_que.put(7)).place(x=213+700, y=550+50, width=150, height=50)
-    
+
     vid_update()
-    
+    sta_update()
     root.mainloop()
 
+
 if __name__ == "__main__":
-    graph_thread=threading.Thread(target=graphMain)
+    graph_thread = threading.Thread(target=graphMain)
     graph_thread.start()
     vid_thread = threading.Thread(target=vid_downstream)
     vid_thread.start()
-    cmd_thread=threading.Thread(target=cmd_upstream)
+    cmd_thread = threading.Thread(target=cmd_upstream)
     cmd_thread.start()
--- core.py
+++ core.py
@@ -1,6 +1,6 @@
 '''
 Author       : Gehrychiang
-LastEditTime : 2022-06-07 12:47:44
+LastEditTime : 2022-06-07 15:19:06
 Website      : www.yilantingfeng.site
 E-mail       : gehrychiang@aliyun.com
 '''
@@ -9,16 +9,18 @@ import socket
 import time
 import cv2
 from PIL import ImageGrab
+from matplotlib.font_manager import json_dump
 import numpy as np
 import json
-import struct
+
 # config area
-vid_port=18081
-cmd_port=18082
-localhost='127.0.0.1'
-ip_addr=localhost
-encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 60]
+vid_port = 18081
+cmd_port = 18082
+localhost = '127.0.0.1'
+ip_addr = localhost
+encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 80]
 # config end
+
 
 def vid_upstream():
     while True:
@@ -32,20 +34,22 @@ def vid_upstream():
                 print('客户端已连接')
                 while True:
                     try:
-                        im=ImageGrab.grab((0,0,854,480))
-                        trans_cv2img = cv2.cvtColor(np.array(im), cv2.COLOR_RGB2RGBA)# for display
+                        im = ImageGrab.grab((0, 0, 854, 480))
+                        trans_cv2img = cv2.cvtColor(
+                            np.array(im), cv2.COLOR_RGB2RGBA)  # for display
                         # cv2.imshow('server_test',trans_cv2img)
                         # key = cv2.waitKey(1) & 0xFF
-                        # if key == ord('q'):  # q键推出
+                        # if key == ord('q'):  # q键退出
                         #     break
                         # !!get a cv mat above!!
 
-                        im_encode=cv2.imencode('.jpg',trans_cv2img)[1]
+                        im_encode = cv2.imencode('.jpg', trans_cv2img,
+                                                 encode_param)[1]
                         data_encode = np.array(im_encode)
                         str_encode = data_encode.tobytes()
                         conn.send(str_encode)
                         # print('sent',len(str_encode))
-                        time.sleep(0.016) # 60fps
+                        time.sleep(0.016)  # 60fps
 
                     except ConnectionResetError:
                         print('连接断开，等待重新连接')
@@ -56,7 +60,8 @@ def vid_upstream():
                 break
 
 def get_status():
-    return "i am ok"
+    # 随机返回一个温湿度
+    return json.dumps({"temp": np.random.randint(20, 30), "humi": np.random.randint(40, 60)})
 
 def cmd_downstream():
     while True:
@@ -70,14 +75,21 @@ def cmd_downstream():
                 print('客户端已连接')
                 while True:
                     try:
-                        req=conn.recv(512)
-                        req_prased=json.loads(req.decode('utf-8'))
-                        print('解析请求：',req_prased["cmd"])
-                        # if req_prased["cmd"]=='getStatus':
-                        #     res=get_status()
-                        #     ret_d=json.dumps({"ret":200,"data":{"status":res}})
-                        #     print(ret_d)
-                        #     conn.send(ret_d.encode('utf-8'))
+                        req = conn.recv(512)
+                        req_prased = json.loads(req.decode('utf-8'))
+                        print('解析请求：', req_prased["cmd"], req_prased["para"])
+                        if req_prased["cmd"] == 'ping':
+                            ret_d = json.dumps({"ret": 200, "data": "pong"})
+                            conn.send(ret_d.encode('utf-8'))
+                        elif req_prased["cmd"] == 'status':
+                            ret_d = json.dumps({"ret": 200, "data": get_status()})
+                            conn.send(ret_d.encode('utf-8'))
+                        else:
+                            ret_d = json.dumps({
+                                "ret": 404,
+                                "data": "not complete"
+                            })
+                            conn.send(ret_d.encode('utf-8'))
 
                     except ConnectionResetError:
                         print('连接断开，等待重新连接')
@@ -86,7 +98,6 @@ def cmd_downstream():
             except Exception:
                 print('监听终止，尝试重启')
                 break
-
 
 
 # def sec_thread_func():
@@ -99,7 +110,7 @@ if __name__ == "__main__":
     vid_thread = threading.Thread(target=vid_upstream)
     vid_thread.start()
     # vid part is done 2022年6月7日09:55:24
-    
+
     cmd_thread = threading.Thread(target=cmd_downstream)
     cmd_thread.start()
     # cmd part is done 2022年6月7日10:17:46
