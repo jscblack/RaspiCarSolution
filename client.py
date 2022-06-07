3cf3d623795f566056612cea40bf70acb0a9d523
jscblack <jscblack@163.com>
2022-06-07 12:48:46 +0800

    gui init

--- client.py
+++ client.py
@@ -1,21 +1,32 @@
 '''
 Author       : Gehrychiang
-LastEditTime : 2022-06-06 22:07:23
+LastEditTime : 2022-06-07 12:48:16
 Website      : www.yilantingfeng.site
 E-mail       : gehrychiang@aliyun.com
 '''
 #客户端
+import tkinter as tk
+import tkinter.ttk as ttk
+import queue
 import socket
 import threading
+from turtle import back, color
 import cv2
 import numpy as np
 import time
+import json
+from PIL import Image, ImageTk
 # config area
 vid_port=18081
 cmd_port=18082
 localhost='127.0.0.1'
 ip_addr=localhost
 # config end
+
+# global area
+que=queue.Queue()
+cmd_que=queue.Queue()
+# global end
 
 def vid_downstream():
     while True:
@@ -24,16 +35,18 @@ def vid_downstream():
         print('服务端已连接')
         while True:
             try:
-                res=client.recv(131072)
+                res=client.recv(262144)
                 # print('已收到服务器信息：', res.decode('utf-8'))
                 # res=client.recv(1024)
-                print('received',len(res))
+                # print('received',len(res))
                 nparr = np.frombuffer(res, dtype='uint8')
-                im = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-                cv2.imshow('client_test',im)
-                key = cv2.waitKey(1) & 0xFF
-                if key == ord('q'):  # q键推出
-                    break
+                img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+                img_tk = img
+                que.put(img_tk)
+                # cv2.imshow('client_test',im)
+                # key = cv2.waitKey(1) & 0xFF
+                # if key == ord('q'):  # q键推出
+                #     break
             except ConnectionResetError:
                 print('连接断开，等待重新连接')
                 client.close()
@@ -43,34 +56,92 @@ def vid_downstream():
                 break
 
 def cmd_upstream():
+    # while True:
+    #     if cmd_que.empty()==False:
+    #         cmd=cmd_que.get()
+    #         print('收到命令：', cmd)
+    #     time.sleep(0.1)
+    client = socket.socket()
+    client.connect((ip_addr, cmd_port))
+    print('服务端已连接')
     while True:
-        client = socket.socket()
-        client.connect((ip_addr, cmd_port))
-        print('服务端已连接')
-        while True:
-            try:
-                # cmd is a json string
-                # {'cmd':'move','para':{'direction':'forward','speed':'0.5'}}
-                # {'cmd':'getStatus','para':{}}
-                # cmd_exp='{"cmd":"getStatus","para":{}}'
-                cmd_exp='cmd_exp_exp'
-                print('发送命令：',cmd_exp)
-                client.send(cmd_exp,encoding='utf-8')
+        try:
+            # cmd is a json-like string
+            # {'cmd':'move','para':{'direction':'forward','speed':'0.5'}}
+            # {'cmd':'getStatus','para':{}}
+            # {"ret": 200, "data": {}}
+            while True:
+                if not cmd_que.empty:
+                    cmd=cmd_que.get()
+                    cmd_d=json.dumps({"cmd":cmd,"para":{}})
+                    client.send(cmd_d.encode('utf-8'))
+                time.sleep(0.1)
 
-                # ret=client.recv(65535)
-                # print('响应结果 ',ret.decode('utf-8'))
-                time.sleep(1)
+            # cmd_exp='{"cmd":"getStatus","para":{}}'
+            # client.send(cmd_exp.encode('utf-8'))
+            # ret=client.recv(512).decode('utf-8')
+            # ret_d=json.loads(ret)
+            # print('响应结果 ',ret_d["data"]["status"])
 
-            except ConnectionResetError:
-                print('连接断开，等待重新连接')
-                client.close()
-                break
-            except Exception:
-                print('连接终止，尝试重启')
-                break
+        except ConnectionResetError:
+            print('连接断开，等待重新连接')
+            client.close()
+            break
+        except Exception:
+            print('连接终止，尝试重启')
+            time.sleep(10)
+            client.close()
+            break
+
+def graphMain():
+    root = tk.Tk()
+    root.title('RaspiCarSolution')
+    root.geometry('1280x720')
+    root.resizable(False, False)
+    root.configure(background='#F0F0F0')
+    label = tk.Label(root, text='RaspiCarSolution',fg='#252526', bg='#F0F0F0', font=('Arial', 20))
+    label.place(x=0, y=0, width=1280, height=50)
+    vid_frame = tk.Label(root)
+    vid_frame.place(x=213, y=55, width=854, height=480)
+    def vid_update():
+        if not que.empty():
+            cv2image= que.get()
+            img = Image.fromarray(cv2image)
+            imgtk = ImageTk.PhotoImage(image = img)
+            vid_frame.imgtk = imgtk
+            vid_frame.configure(image=imgtk)
+            # 15ms后重复以连续捕获
+        vid_frame.after(10, vid_update)
     
+    # 四个方向键按钮
+    forward_but=tk.Button(root, text='↑',font=('Arial', 16), command=lambda: cmd_que.put(1)).place(x=213+50, y=550, width=50, height=50)
+    backward_but=tk.Button(root, text='↓',font=('Arial', 16), command=lambda: cmd_que.put(2)).place(x=213+50, y=550+100, width=50, height=50)
+    left_but=tk.Button(root, text='←',font=('Arial', 16), command=lambda: cmd_que.put(3)).place(x=213, y=550+50, width=50, height=50)
+    right_but=tk.Button(root, text='→',font=('Arial', 16), command=lambda: cmd_que.put(4)).place(x=213+100, y=550+50, width=50, height=50)
+    # 方向键捕获
+    root.bind('<KeyPress-Up>', lambda event: cmd_que.put(1))
+    root.bind('<KeyPress-Down>', lambda event: cmd_que.put(2))
+    root.bind('<KeyPress-Left>', lambda event: cmd_que.put(3))
+    root.bind('<KeyPress-Right>', lambda event: cmd_que.put(4))
+    root.bind('<KeyPress-w>', lambda event: cmd_que.put(1))
+    root.bind('<KeyPress-d>', lambda event: cmd_que.put(2))
+    root.bind('<KeyPress-a>', lambda event: cmd_que.put(3))
+    root.bind('<KeyPress-s>', lambda event: cmd_que.put(4))
+    # 运行模式选择
+    mode_label=tk.Label(root, text='Running Mode',fg='#252526', bg='#F0F0F0', font=('Arial', 12))
+    mode_label.place(x=213+300, y=550+50, width=200, height=50)
+    manual_but=tk.Radiobutton(root, text='Manual',font=('Arial', 12),  value=1, command=lambda: cmd_que.put(5)).place(x=213+500, y=550+50, width=100, height=50)
+    auto1_but=tk.Radiobutton(root, text='Auto-Lane',font=('Arial', 12),  value=2, command=lambda: cmd_que.put(6)).place(x=213+600, y=550+50, width=100, height=50)
+    auto2_but=tk.Radiobutton(root, text='Auto-Avoidance',font=('Arial', 12),  value=3, command=lambda: cmd_que.put(7)).place(x=213+700, y=550+50, width=150, height=50)
+    
+    vid_update()
+    
+    root.mainloop()
+
 if __name__ == "__main__":
-    # vid_thread = threading.Thread(target=vid_downstream)
-    # vid_thread.start()
-    cmd_upstream=threading.Thread(target=cmd_upstream)
-    cmd_upstream.start()
+    graph_thread=threading.Thread(target=graphMain)
+    graph_thread.start()
+    vid_thread = threading.Thread(target=vid_downstream)
+    vid_thread.start()
+    cmd_thread=threading.Thread(target=cmd_upstream)
+    cmd_thread.start()
--- core.py
+++ core.py
@@ -1,6 +1,6 @@
 '''
 Author       : Gehrychiang
-LastEditTime : 2022-06-06 22:07:03
+LastEditTime : 2022-06-07 12:47:44
 Website      : www.yilantingfeng.site
 E-mail       : gehrychiang@aliyun.com
 '''
@@ -11,6 +11,7 @@ import cv2
 from PIL import ImageGrab
 import numpy as np
 import json
+import struct
 # config area
 vid_port=18081
 cmd_port=18082
@@ -31,19 +32,20 @@ def vid_upstream():
                 print('客户端已连接')
                 while True:
                     try:
-                        im=ImageGrab.grab((0,0,640,320))
-                        trans_cv2img = cv2.cvtColor(np.array(im), cv2.COLOR_RGB2BGR)
+                        im=ImageGrab.grab((0,0,854,480))
+                        trans_cv2img = cv2.cvtColor(np.array(im), cv2.COLOR_RGB2RGBA)# for display
                         # cv2.imshow('server_test',trans_cv2img)
                         # key = cv2.waitKey(1) & 0xFF
                         # if key == ord('q'):  # q键推出
                         #     break
-                        # get a cv mat above
+                        # !!get a cv mat above!!
+
                         im_encode=cv2.imencode('.jpg',trans_cv2img)[1]
                         data_encode = np.array(im_encode)
                         str_encode = data_encode.tobytes()
                         conn.send(str_encode)
                         # print('sent',len(str_encode))
-                        time.sleep(0.02) # 60fps
+                        time.sleep(0.016) # 60fps
 
                     except ConnectionResetError:
                         print('连接断开，等待重新连接')
@@ -68,12 +70,14 @@ def cmd_downstream():
                 print('客户端已连接')
                 while True:
                     try:
-                        req=conn.recv(64)
-                        print('收到请求：',req)
-                        # req_prased=json.load(req.decode('utf-8'))
-                        # if req_prased['cmd']=='getStatus':
+                        req=conn.recv(512)
+                        req_prased=json.loads(req.decode('utf-8'))
+                        print('解析请求：',req_prased["cmd"])
+                        # if req_prased["cmd"]=='getStatus':
                         #     res=get_status()
-                        #     conn.send(res,encoding='utf-8')
+                        #     ret_d=json.dumps({"ret":200,"data":{"status":res}})
+                        #     print(ret_d)
+                        #     conn.send(ret_d.encode('utf-8'))
 
                     except ConnectionResetError:
                         print('连接断开，等待重新连接')
@@ -82,19 +86,24 @@ def cmd_downstream():
             except Exception:
                 print('监听终止，尝试重启')
                 break
-    
+
+
+
 # def sec_thread_func():
 #     # TODO
-    
+
 # def thd_thread_func():
 #     # TODO
 
-
 if __name__ == "__main__":
-    # vid_thread = threading.Thread(target=vid_upstream)
-    # vid_thread.start()
+    vid_thread = threading.Thread(target=vid_upstream)
+    vid_thread.start()
+    # vid part is done 2022年6月7日09:55:24
+    
     cmd_thread = threading.Thread(target=cmd_downstream)
     cmd_thread.start()
+    # cmd part is done 2022年6月7日10:17:46
+
     # sec_thread = threading.Thread(target=sec_thread_func)
     # thd_thread = threading.Thread(target=thd_thread_func)
     # sec_thread.start()
